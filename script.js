8fd638a054b4f0df1abd01c0f6d8bfa72ae978be
Foad Botan <foadbotan@gmail.com>
2023-06-07 12:56:10 +1000

    Create add key functionality and style tiles

--- script.js
+++ script.js
@@ -2,7 +2,55 @@ import { validWords } from "./data.js";
 
 const alertContainer = document.getElementById("alert-container");
 const rows = document.querySelectorAll(".row");
+const keyboardButtons = document.querySelectorAll(".keyboard-button");
 
 const CORRECT_WORD = getRandomWord(validWords);
+const MAX_WORD_LENGTH = CORRECT_WORD.length;
 
+let currentRowIndex = 0;
+let currentTileIndex = 0;
 console.log(CORRECT_WORD);
+
+document.addEventListener("keydown", handleKeyPressOrButton);
+[...keyboardButtons].forEach((button) =>
+  button.addEventListener("click", handleKeyPressOrButton)
+);
+
+function handleKeyPressOrButton(e) {
+  const key = e.key ? e.key : e.target.dataset.key;
+  const isInvalidKey = !/^[a-z]$|^Backspace$|^Enter$/.test(key);
+  if (isInvalidKey) return;
+
+  processKey(key);
+}
+
+function getRandomWord(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function processKey(char) {
+  const currentRow = rows[currentRowIndex].children;
+  const currentTile = currentRow[currentTileIndex];
+  const previousTile = currentRow[currentTileIndex - 1];
+
+  if (char === "Backspace") {
+    if (currentTileIndex === 0) return;
+    previousTile.textContent = "";
+    previousTile.classList.remove("active");
+    currentTileIndex--;
+    return;
+  }
+
+  if (char === "Enter") {
+    return;
+  }
+
+  if (currentTileIndex === MAX_WORD_LENGTH) {
+    return;
+  }
+
+  currentTile.textContent = char;
+  currentTile.classList.add("active");
+  currentTileIndex++;
+}
