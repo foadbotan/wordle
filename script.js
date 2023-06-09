2191b154706d716007fd5de271192ecf2043b4c0
Foad Botan <foadbotan@gmail.com>
2023-06-09 22:23:33 +1000

    feat(#checkAnswer): create checkAnswer function

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const [...keyboardButtons] = document.querySelectorAll(".keyboard-button");
 
 const CORRECT_WORD = getRandomElement(validWords);
 const CURRENT_INDEX = { row: 0, tile: 0 };
+const MAX_ROUNDS = rows.length;
+let isGameOver = false;
 
 let answer = "";
 
@@ -24,46 +26,84 @@ keyboardButtons.forEach(button => {
 });
 
 function handleKeyPress(key) {
-  const tiles = getCurrentRow();
-  const currentTile = tiles[CURRENT_INDEX.tile];
-  const previousTile = tiles[CURRENT_INDEX.tile - 1];
-
-  if (key === "Backspace") deleteTile(previousTile);
+  if (isGameOver) return;
+  if (key === "Backspace") deleteTile();
   if (key === "Enter") checkAnswer();
-  if (key.match(/^[a-z]$/)) addTile(currentTile, key);
+  if (key.match(/^[a-z]$/)) addTile(key);
 }
 
 function deleteTile(tile) {
-  const isFirstTile = CURRENT_INDEX.tile === 0;
-  if (isFirstTile) return;
+  const previousTile = getPreviousTile();
+  if (!previousTile) return;
 
   answer = answer.slice(0, -1);
-  tile.textContent = "";
-  tile.classList.remove("active");
+  previousTile.textContent = "";
+  previousTile.classList.remove("filled");
   CURRENT_INDEX.tile--;
 }
 
-function addTile(tile, key) {
-  const isLastTile = CURRENT_INDEX.tile === CORRECT_WORD.length;
-  if (isLastTile) return;
+function addTile(key) {
+  const currentTile = getCurrentTile();
+  const isRowFull = CURRENT_INDEX.tile === CORRECT_WORD.length;
+  if (isRowFull) return;
 
   answer += key.toUpperCase();
-  tile.textContent = key;
-  tile.classList.add("active");
+  currentTile.textContent = key;
+  currentTile.classList.add("filled");
   CURRENT_INDEX.tile++;
 }
 
 function checkAnswer(tiles) {
-  console.log("checking: ", answer);
+  const isTooShort = answer.length !== CORRECT_WORD.length;
+  if (isTooShort) {
+    showAlert("Not enough letters");
+    return;
+  }
+  const isInvalidWord = !validWords.includes(answer);
+  if (isInvalidWord) {
+    showAlert("Not a valid word");
+    return;
+  }
+
+  const gameOver = CURRENT_INDEX.row === MAX_ROUNDS;
+  flipTiles();
+  if (answer === CORRECT_WORD || gameOver) endGame();
+  moveToNextRow();
+}
+
+function endGame() {
+  answer === CORRECT_WORD ? showAlert("You won!") : showAlert("You lost!");
+  isGameOver = true;
+}
+
+function flipTiles() {
+  console.log("flipping tiles");
+}
+
+function showAlert(message) {
+  console.log(message);
+}
+
+function moveToNextRow() {
+  answer = "";
+  CURRENT_INDEX.tile = 0;
+  CURRENT_INDEX.row++;
+}
+
+function getCurrentTile() {
+  const currentRow = getCurrentRow();
+  return currentRow[CURRENT_INDEX.tile];
+}
+
+function getPreviousTile() {
+  const currentRow = getCurrentRow();
+  return currentRow[CURRENT_INDEX.tile - 1];
 }
 
 function getCurrentRow() {
   return rows[CURRENT_INDEX.row].children;
 }
 
-/**--------------------------------------------
- *               Helper Functions
- *---------------------------------------------**/
 function getRandomElement(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
