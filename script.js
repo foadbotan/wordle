db5709fafc0047d9db683b70ce169a3e19eae3d5
Foad Botan <foadbotan@gmail.com>
2023-06-09 18:27:37 +1000

    refactor(script.js): simplified event handlers on keypress

--- script.js
+++ script.js
@@ -2,55 +2,69 @@ import { validWords } from "./data.js";
 
 const alertContainer = document.getElementById("alert-container");
 const rows = document.querySelectorAll(".row");
-const keyboardButtons = document.querySelectorAll(".keyboard-button");
+const [...keyboardButtons] = document.querySelectorAll(".keyboard-button");
 
-const CORRECT_WORD = getRandomWord(validWords);
-const MAX_WORD_LENGTH = CORRECT_WORD.length;
+const CORRECT_WORD = getRandomElement(validWords);
+const CURRENT_INDEX = { row: 0, tile: 0 };
 
-let currentRowIndex = 0;
-let currentTileIndex = 0;
+let answer = "";
+
 console.log(CORRECT_WORD);
 
-document.addEventListener("keydown", handleKeyPressOrButton);
-[...keyboardButtons].forEach((button) =>
-  button.addEventListener("click", handleKeyPressOrButton)
-);
+document.addEventListener("keydown", e => {
+  const key = e.key;
+  handleKeyPress(key);
+});
 
-function handleKeyPressOrButton(e) {
-  const key = e.key ? e.key : e.target.dataset.key;
-  const isInvalidKey = !/^[a-z]$|^Backspace$|^Enter$/.test(key);
-  if (isInvalidKey) return;
+keyboardButtons.forEach(button => {
+  button.addEventListener("click", e => {
+    const key = e.target.dataset.key;
+    handleKeyPress(key);
+  });
+});
 
-  processKey(key);
+function handleKeyPress(key) {
+  const tiles = getCurrentRow();
+  const currentTile = tiles[CURRENT_INDEX.tile];
+  const previousTile = tiles[CURRENT_INDEX.tile - 1];
+
+  if (key === "Backspace") deleteTile(previousTile);
+  if (key === "Enter") checkAnswer();
+  if (key.match(/^[a-z]$/)) addTile(currentTile, key);
 }
 
-function getRandomWord(array) {
+function deleteTile(tile) {
+  const isFirstTile = CURRENT_INDEX.tile === 0;
+  if (isFirstTile) return;
+
+  answer = answer.slice(0, -1);
+  tile.textContent = "";
+  tile.classList.remove("active");
+  CURRENT_INDEX.tile--;
+}
+
+function addTile(tile, key) {
+  const isLastTile = CURRENT_INDEX.tile === CORRECT_WORD.length;
+  if (isLastTile) return;
+
+  answer += key.toUpperCase();
+  tile.textContent = key;
+  tile.classList.add("active");
+  CURRENT_INDEX.tile++;
+}
+
+function checkAnswer(tiles) {
+  console.log("checking: ", answer);
+}
+
+function getCurrentRow() {
+  return rows[CURRENT_INDEX.row].children;
+}
+
+/**--------------------------------------------
+ *               Helper Functions
+ *---------------------------------------------**/
+function getRandomElement(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
-}
-
-function processKey(char) {
-  const currentRow = rows[currentRowIndex].children;
-  const currentTile = currentRow[currentTileIndex];
-  const previousTile = currentRow[currentTileIndex - 1];
-
-  if (char === "Backspace") {
-    if (currentTileIndex === 0) return;
-    previousTile.textContent = "";
-    previousTile.classList.remove("active");
-    currentTileIndex--;
-    return;
-  }
-
-  if (char === "Enter") {
-    return;
-  }
-
-  if (currentTileIndex === MAX_WORD_LENGTH) {
-    return;
-  }
-
-  currentTile.textContent = char;
-  currentTile.classList.add("active");
-  currentTileIndex++;
 }
